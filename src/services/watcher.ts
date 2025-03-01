2813b2cc6279e103df8eba304f91d4abdec9ad20
kian1991 <kianluetke@gmail.com>
2025-03-01 20:02:23 +0100

    feat: update transaction handling to watch for new blocks and improve database insertion logic

--- src/services/watcher.ts
+++ src/services/watcher.ts
@@ -1,10 +1,18 @@
-import { ethers } from 'ethers';
+import { BlockTag, ethers, Provider } from 'ethers';
 import { db } from '../../db/client';
 import { subscriptions, transactions } from '../../db/schema';
-import { eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
 import { ALCHEMY_WS_URL } from '../constants';
 
 const provider = new ethers.WebSocketProvider(ALCHEMY_WS_URL);
+
+// provider.on('connect', () => {
+//   console.log('Connected to Alchemy');
+// });
+
+// provider.on('error', (error) => {
+//   console.error('Error', error);
+// });
 
 let watchedSubscriptions = new Map<string, string>();
 
@@ -15,6 +23,7 @@ async function loadwatchedSubscriptions() {
     .where(eq(subscriptions.isActive, true));
 
   watchedSubscriptions.clear();
+
   addresses.forEach(({ id, address }) => {
     watchedSubscriptions.set(address, id);
   });
@@ -23,37 +32,49 @@ async function loadwatchedSubscriptions() {
 }
 
 export async function startWatching() {
-  await loadwatchedSubscriptions(); // Load watched addresses on startup
+  await loadwatchedSubscriptions();
 
-  provider.on('pending', async (txHash) => {
-    try {
-      const tx = await provider.getTransaction(txHash);
-      // console.log('Pending transaction:', tx?.to, tx?.from);
-      if (
-        tx &&
-        tx.to &&
-        tx.from &&
-        (watchedSubscriptions.has(tx.to) || watchedSubscriptions.has(tx.from))
-      ) {
-        console.log(`Transaction detected: ${txHash}`);
+  provider.on('block', async (blockNumber: BlockTag) => {
+    console.log(`New block: ${blockNumber.toString()}`);
 
-        const id =
-          watchedSubscriptions.get(tx.to) || watchedSubscriptions.get(tx.from);
+    const block = await provider.getBlock(blockNumber);
 
-        await db.insert(transactions).values({
-          subscriptionId: id as string, // Assertion bc we know it exists from the if condition
-          id: tx.hash,
-          from: tx.from,
-          to: tx.to,
-          value: ethers.formatEther(tx.value),
-          timestamp: new Date(),
-        });
+    if (!block) {
+      console.log('Block not found, skipping');
+      return;
+    }
+
+    for (const hash of block.transactions) {
+      const tx = await provider.getTransaction(hash);
+      if (!tx) continue; // Skip if transaction not found
+      if (tx.to && watchedSubscriptions.has(tx.to)) {
+        const id = watchedSubscriptions.get(tx.to);
+        if (!id) continue; // Skip if subscription not found
+        const existingTx = await db
+          .select()
+          .from(transactions)
+          .where(
+            and(
+              eq(transactions.id, tx.hash),
+              eq(transactions.subscriptionId, id)
+            )
+          );
+        if (existingTx.length > 0) continue; // transaction already exists
+        await db
+          .insert()
+          .into(transactions)
+          .values({
+            id: tx.hash,
+            subscriptionId: id,
+            from: tx.from,
+            to: tx.to,
+            value: tx.value.toString(),
+            timestamp: new Date(block.timestamp * 1000),
+          });
+        console.log(`New transaction detected: ${tx.hash}`);
       }
-    } catch (error) {
-      console.error('Error processing transaction:', error);
     }
   });
 
-  // Load watched addresses every minute
-  setInterval(loadwatchedSubscriptions, 1 * 60 * 1000);
+  setInterval(loadwatchedSubscriptions, 1000 * 60 * 1); // 1 minute
 }
